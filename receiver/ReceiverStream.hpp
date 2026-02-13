3a3e7f4d65130b5273cffbd4380deafb016e8972
parkmin <samsungplayee@gmail.com>
2026-02-13 14:58:14 -0600

    E

--- receiver/ReceiverStream.hpp
+++ receiver/ReceiverStream.hpp
@@ -313,7 +313,7 @@ namespace receiver {
             settings.es_allow_migration = 0;
             settings.es_pace_packets = 0;
             settings.es_delayed_acks = 0;
-            settings.es_max_batch_size = 256;
+            settings.es_max_batch_size = 32;
             settings.es_rw_once = 1;
 
             char err_buf[256];
