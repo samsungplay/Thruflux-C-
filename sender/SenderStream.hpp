22f45e85307a0a1e492ab56bf2c8e85874036311
parkmin <samsungplayee@gmail.com>
2026-02-16 11:36:55 -0600

    E

--- sender/SenderStream.hpp
+++ sender/SenderStream.hpp
@@ -354,7 +354,7 @@ namespace sender {
             settings.es_init_max_stream_data_bidi_remote = SenderConfig::quicStreamWindowBytes;
             settings.es_handshake_to = 30000000;
             settings.es_allow_migration = 0;
-            settings.es_pace_packets = 0;
+            settings.es_pace_packets = 1;
             settings.es_delayed_acks = 0;
             settings.es_max_batch_size = 32;
             settings.es_scid_len = 8;
