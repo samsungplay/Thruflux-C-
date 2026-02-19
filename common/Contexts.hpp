de9a78b12a70201e08bbc54e42f06d4c2b5d56fd
parkmin <samsungplayee@gmail.com>
2026-02-19 15:27:17 -0600

    E

--- common/Contexts.hpp
+++ common/Contexts.hpp
@@ -26,7 +26,7 @@ namespace llfio = LLFIO_V2_NAMESPACE;
 namespace common {
     inline constexpr char RECEIVER_MANIFEST_RECEIVED_ACK = 0x06;
     inline constexpr char RECEIVER_TRANSFER_COMPLETE_ACK = 0x07;
-    inline static constexpr uint64_t CHUNK_SIZE = 2 * 1024 * 1024; //controls disk io buffer size
+    inline static constexpr uint64_t CHUNK_SIZE = 4 * 1024 * 1024; //controls disk io buffer size
 
     struct FileHandleCache {
         struct Entry {
