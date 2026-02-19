e28e1634ccdd69ffc79f92b94a983796dac72e28
parkmin <samsungplayee@gmail.com>
2026-02-19 10:12:01 -0600

    E

--- sender/SenderStream.hpp
+++ sender/SenderStream.hpp
@@ -359,7 +359,7 @@ namespace sender {
             settings.es_allow_migration = 0;
             settings.es_pace_packets = 1;
             settings.es_delayed_acks = 0;
-            settings.es_max_batch_size = 64;
+            settings.es_max_batch_size = 32;
             settings.es_scid_len = 8;
             settings.es_max_cfcw = SenderConfig::quicConnWindowBytes * 2;
             settings.es_max_sfcw = SenderConfig::quicStreamWindowBytes * 2;
