55ad80dfd5cb16b6bc916dddf68bde9e530e3552
parkmin <samsungplayee@gmail.com>
2026-02-19 10:09:51 -0600

    E

--- receiver/ReceiverStream.hpp
+++ receiver/ReceiverStream.hpp
@@ -460,9 +460,9 @@ namespace receiver {
             settings.es_init_max_stream_data_bidi_remote = ReceiverConfig::quicStreamWindowBytes;
             settings.es_handshake_to = 16777215;
             settings.es_allow_migration = 0;
-            settings.es_pace_packets = 0;
+            settings.es_pace_packets = 1;
             settings.es_delayed_acks = 0;
-            settings.es_max_batch_size = 32;
+            settings.es_max_batch_size = 64;
             settings.es_scid_len = 8;
             settings.es_max_cfcw = ReceiverConfig::quicConnWindowBytes * 2;
             settings.es_max_sfcw = ReceiverConfig::quicStreamWindowBytes * 2;
--- sender/SenderStream.hpp
+++ sender/SenderStream.hpp
@@ -357,9 +357,9 @@ namespace sender {
             settings.es_init_max_stream_data_bidi_remote = SenderConfig::quicStreamWindowBytes;
             settings.es_handshake_to = 30000000;
             settings.es_allow_migration = 0;
-            settings.es_pace_packets = 0;
+            settings.es_pace_packets = 1;
             settings.es_delayed_acks = 0;
-            settings.es_max_batch_size = 32;
+            settings.es_max_batch_size = 64;
             settings.es_scid_len = 8;
             settings.es_max_cfcw = SenderConfig::quicConnWindowBytes * 2;
             settings.es_max_sfcw = SenderConfig::quicStreamWindowBytes * 2;
